62878b2015d67b427101e6ba9577d654cd78587b
jayven <kassarar@gmail.com>
2020-08-22 18:00:20 +0800

    添加 APIError

--- utils/api_result.go
+++ utils/api_result.go
@@ -1,5 +1,9 @@
 package utils
 
+import (
+	"fmt"
+)
+
 // APIResultBase 是 api 返回结果基础字段
 type APIResultBase struct {
 	// Code 是错码，非 0 表示错误
@@ -7,4 +11,19 @@ type APIResultBase struct {
 
 	// Msg 是错误描述
 	Msg string `json:"msg"`
+}
+
+// APIError 是 API 错误
+type APIError APIResultBase
+
+// ResultError 在 Code 非 0 的时候返回非 nil error
+func (result *APIResultBase) ResultError() error {
+	if result.Code == 0 {
+		return nil
+	}
+	return (*APIError)(result)
+}
+
+func (e *APIError) Error() string {
+	return fmt.Sprintf("%d: %s", e.Code, e.Msg)
 }
