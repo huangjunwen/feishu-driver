af4a6278bfd4008eaa00c2da3d7c55b114ae8897
jayven <kassarar@gmail.com>
2020-08-26 12:26:29 +0800

    添加函数形式的 provider

--- conf/provider.go
+++ conf/provider.go
@@ -17,3 +17,22 @@ type AppTicketProvider interface {
 	// FeishuAppTicket 返回 app ticket 或错误
 	FeishuAppTicket() (string, error)
 }
+
+// AppAccessTokenProviderFunc 是函数形式的 AppAccessTokenProvider
+type AppAccessTokenProviderFunc func() (string, error)
+
+// TenantAccessTokenProviderFunc 是函数形式的 TenantAccessTokenProvider
+type TenantAccessTokenProviderFunc func() (string, error)
+
+// AppTicketProviderFunc 是函数形式的 AppTicketProvider
+type AppTicketProviderFunc func() (string, error)
+
+var (
+	_ AppAccessTokenProvider    = (AppAccessTokenProviderFunc)(nil)
+	_ TenantAccessTokenProvider = (TenantAccessTokenProviderFunc)(nil)
+	_ AppTicketProvider         = (AppTicketProviderFunc)(nil)
+)
+
+func (f AppAccessTokenProviderFunc) FeishuAppAccessToken() (string, error)       { return f() }
+func (f TenantAccessTokenProviderFunc) FeishuTenantAccessToken() (string, error) { return f() }
+func (f AppTicketProviderFunc) FeishuAppTicket() (string, error)                 { return f() }
